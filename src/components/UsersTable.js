6625f908de7fa73b084220fce1c4b8991f662c03
Raven <48630934+RaquelWriter@users.noreply.github.com>
2022-10-18 23:26:46 +0200

    Minor changes

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -1,24 +1,21 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import '../stylesheets/UsersTable.css';
 
-function UsersTable(){
-
+function UsersTable() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch ('https://jsonplaceholder.typicode.com/users')
-      .then (response => response.json())
-      .then (json => setData(json))
-      .catch((err) => console.log('ERROR FETCHING FROM JSON ' + err))
-  }, [])
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((err) => console.log('ERROR FETCHING FROM JSON ' + err));
+  }, []);
 
-  const [selectedCity, setSelectedCity] = useState ([]);
+  const [selectedCity, setSelectedCity] = useState([]);
   const handleUsersTable = (event) => {
-    return (
-        setSelectedCity(event.target.value)
-    );
-  }
-/*
+    return setSelectedCity(event.target.value);
+  };
+  /*
   const [showAllUsers, setShowAllUsers] = useState(true);
   const handleShowAll = () => {
     return(
@@ -31,43 +28,51 @@ function UsersTable(){
     <>
       <div className='container'>
         <h1>USERS DATABASE</h1>
-          <form className='form-container'>
-            <label className='filterByCityText'>Filter by city:</label>
-              <select onChange= {handleUsersTable}>
-                    <option value="">All</option>
-               {data.map((item) => {
-                  return (
-                    <option value={item.address.city}>{item.address.city}</option>
-                  );
-                })}
-              </select> 
-          </form>
-      <div className="table">
-          {data.filter((item) => selectedCity.length !== 0 ? item.address.city === selectedCity : item)
+        <form className='form-container'>
+          <label className='filterByCityText'>Filter by city:</label>
+          <select onChange={handleUsersTable}>
+            <option value=''>All</option>
+            {data.map((item) => {
+              return (
+                <option value={item.address.city}>{item.address.city}</option>
+              );
+            })}
+          </select>
+        </form>
+        <div className='table'>
+          {data
+            .filter((item) =>
+              selectedCity.length !== 0
+                ? item.address.city === selectedCity
+                : item
+            )
             .map((item) => {
-          /*{data.map(item =>{ */
-            return (
-              <>
-                <tr key={item.id}>
-                  <th colSpan="2">ID #{item.id} | Name: {item.name}</th>
-                </tr>
-                <tr>
-                  <td>Username</td>
-                  <td>{item.username}</td>
-                </tr>
-                <tr>
-                  <td>E-mail</td>
-                  <td>{item.email}</td>
-                </tr>
-                <tr>
-                  <td>Address</td>
-                  <td>{item.address.street}, {item.address.city}</td>
-                </tr>
-              </>
-            );
-          })}
-
-      </div>
+              /*{data.map(item =>{ */
+              return (
+                <>
+                  <tr key={item.id}>
+                    <th colSpan='2'>
+                      ID #{item.id} | Name: {item.name}
+                    </th>
+                  </tr>
+                  <tr>
+                    <td>Username</td>
+                    <td>{item.username}</td>
+                  </tr>
+                  <tr>
+                    <td>E-mail</td>
+                    <td>{item.email}</td>
+                  </tr>
+                  <tr>
+                    <td>Address</td>
+                    <td>
+                      {item.address.street}, {item.address.city}
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
+        </div>
       </div>
     </>
   );
